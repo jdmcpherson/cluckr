027d2d83a1c2099f0ca6e89dad9b0c8b14e2834e
James McPherson <mcpherson.jamesd@gmail.com>
2017-10-10 09:38:08 -0700

    Initalised morgan logger

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const Express = require('express');
+const morgan = require('morgan')
 
 const app = Express();
+app.use(morgan('dev'));
 
 app.get('/', (request, response) => {
     response.send('test')
